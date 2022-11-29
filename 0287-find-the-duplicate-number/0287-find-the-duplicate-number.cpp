55e957aed04f749280c7b1c22de340c59c243b80
Abhishek Verma <75570830+abhishekverma1289@users.noreply.github.com>
2022-11-29 14:47:00 +0530

    Time: 176 ms (78.84%), Space: 61.2 MB (62.21%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,7 +4,7 @@ public:
 sort(nums.begin(),nums.end());
 for(int i=0;i<nums.size();i++)
 {
-    if(nums[i]==nums[i+1]) return nums[i];
+    if((nums[i]^nums[i+1])==0) return nums[i];
 }
 return 0;
     }
