4c872478d1adc838ba8dfdb0a57747473838e423
Abhishek Verma <75570830+abhishekverma1289@users.noreply.github.com>
2026-08-20 03:23:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (88.72%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -6,8 +6,8 @@ class Solution {
         while(start < end){
             int mid = start + (end - start)/2;
 
-            if(nums[mid] < nums[mid+1]) start = mid + 1;
-            else end = mid;
+            if(nums[mid] < nums[mid+1]) start = mid + 1;        // will never go out of bounds bcs start < end , so mid is always < end;
+            else end = mid;   //when search shrink in left start = 0 end = 1 , we either move end = mid , so loop stops that why we dont do mid - 1; beautiful part nigger
         }
         return start;
     }
