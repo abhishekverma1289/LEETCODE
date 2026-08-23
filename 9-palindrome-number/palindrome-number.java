895a6ed8d4f2cc45890c473b0345a38dae939b6a
Abhishek Verma <75570830+abhishekverma1289@users.noreply.github.com>
2026-08-23 17:54:55 +0530

    Time: 5 ms (81.83%) | Memory: 46.1 MB (16.38%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isPalindrome(int number) {
         int reverseNumber = 0;
-        int numCopy = number;
+        int numberCopy = number;
 
         while(number > 0){
             int remainder = number % 10;
             reverseNumber = (reverseNumber * 10) + remainder;
             number = number / 10;
         }
-        if(reverseNumber == numCopy) return true;
+        if(reverseNumber == numberCopy) return true;
         else return false;
     }
 }
