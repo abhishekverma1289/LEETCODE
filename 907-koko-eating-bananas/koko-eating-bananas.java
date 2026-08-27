7ff2a43d351af8c397ec6252dbb5c794288cd8ce
Abhishek Verma <75570830+abhishekverma1289@users.noreply.github.com>
2026-08-27 22:26:07 +0530

    Time: 10 ms (53.41%) | Memory: 47.9 MB (58.94%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,23 +1,21 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-     int start = 1;
-     int end = Arrays.stream(piles).max().orElseThrow();
+       int start = 1;
+       int end = Arrays.stream(piles).max().orElseThrow();
 
-     while(start < end){
+       while(start < end){
         int mid = start + (end - start)/2;
         int count = 0;
 
-        for(int banana :piles){
-            count += (banana + mid - 1) / mid;
+        for(int banana: piles){
+            count += (banana + mid - 1)/mid;
         }
 
-        if(count > h){
-            start = mid + 1;
-        }
-        else{
-            end = mid;
-        }
+       if(count > h){ /// equal or chota
+        start = mid + 1;
+       }
+       else end = mid;
+       } 
+       return start;
     }
-    return start;
- }
 }
