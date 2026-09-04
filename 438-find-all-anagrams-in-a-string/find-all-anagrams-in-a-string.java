f0c4474cbc5ac4dc539e6854101639dff731e44c
Abhishek Verma <75570830+abhishekverma1289@users.noreply.github.com>
2026-09-04 21:51:05 +0530

    Time: 12 ms (52.23%) | Memory: 46.9 MB (58.11%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -2,21 +2,24 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
        ArrayList<Integer> list = new ArrayList<>();
 
+       //is p is bigger how can s contain it , in any order possible
        if(p.length() > s.length()) return list;
 
        int[] required = new int[26];
        int[] window = new int[26];
 
+       //create a required frequency Array
        for(char c : p.toCharArray()){
         required[c - 'a']++;
        }
 
-       // creating a Window
+       // creating a Window Frequency Array
        for(int i = 0; i<p.length(); i++){
         char ch = s.charAt(i);
         window[ch - 'a']++;
        } 
 
+       //at index 0 , if first window is valid answer
        if(Arrays.equals(window, required)) list.add(0);
 
        for(int high = p.length(); high<s.length(); high++){
