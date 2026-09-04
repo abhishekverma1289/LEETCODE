42e94540c04a04817e2fe4bcf44e5ba838a3d841
Abhishek Verma <75570830+abhishekverma1289@users.noreply.github.com>
2026-09-04 14:54:13 +0530

    Time: 1 ms (99.79%) | Memory: 46.4 MB (49.36%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -1,9 +1,9 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
+        
         int[] rightMin = new int[n];
         int min = nums[n-1];
-        int leftMax = nums[0];
 
         for(int i = n-1; i >= 0; i--){
             if(nums[i] <= min){
@@ -12,8 +12,7 @@ class Solution {
             rightMin[i] = min;
         }
 
-        int index = -1;
-        int leastStable = Integer.MAX_VALUE;
+        int leftMax = nums[0];
 
         for(int i=0; i<n; i++){
             if(nums[i] >= leftMax) {
@@ -21,11 +20,9 @@ class Solution {
             }
 
             if(leftMax - rightMin[i] <= k){
-                leastStable = leftMax - rightMin[i];
-                index = i;
-                break;
+                return i;
             }
         }
-        return index;
+        return -1;
     }
 }
