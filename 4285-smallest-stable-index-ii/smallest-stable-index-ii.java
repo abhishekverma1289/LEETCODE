171442a2ce14d056de30d181b4ca8c3bb3eddd29
Abhishek Verma <75570830+abhishekverma1289@users.noreply.github.com>
2026-09-05 12:44:35 +0530

    Time: 4 ms (88.55%) | Memory: 133.2 MB (20.99%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -7,9 +7,8 @@ class Solution {
       for(int i = n - 2; i >= 0; i--){
         rightMin[i] = Math.min(rightMin[i+1] , nums[i]);
       }
-      int index = -1;
+
       int leftMax = nums[0];
-      int mostStable = Integer.MAX_VALUE;
 
       for(int i = 0; i < n; i++){
         leftMax = Math.max(leftMax , nums[i]);
