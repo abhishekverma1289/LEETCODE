e4bddadced4cc34ff486021b77cac310bec0fde3
Abhishek Verma <75570830+abhishekverma1289@users.noreply.github.com>
2026-09-24 23:15:54 +0530

    Time: 33 ms (70.02%) | Memory: 60.5 MB (7.13%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,38 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-         Arrays.sort(nums);
-         List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
 
-         for(int start = 0; start<nums.length - 2; start++){
-            //skip the start duplicates
-            if (start > 0 && nums[start] == nums[start - 1]) {
-                continue;
-            }
+        for(int start = 0; start < nums.length - 2; start++){
+            while(start > 0 && start < nums.length && nums[start] == nums[start - 1]) start++;
 
-            int end = nums.length - 1;
             int mid = start + 1;
+            int end = nums.length - 1;
 
             while(mid < end){
                 int sum = nums[start] + nums[mid] + nums[end];
-                if(sum > 0) end--;
-                else if(sum < 0) mid++;
-                else{
-                    list.add(Arrays.asList(nums[start] , nums[mid] , nums[end]));
 
-                 // Skip duplicate mid values
-                    while (mid < end && nums[mid] == nums[mid + 1]) {
-                        mid++;
-                    }
+                if(sum == 0){
+                    List<Integer> ansList = new ArrayList<>();
+                    ansList.add(nums[start]);
+                    ansList.add(nums[mid]);
+                    ansList.add(nums[end]);
 
-                    // Skip duplicate end values
-                    while (mid < end && nums[end] == nums[end - 1]) {
-                        end--;
-                    }
-                    
+                    list.add(ansList);
+
+                    while(mid < end && nums[mid] == nums[mid + 1]) mid++;
+                    while(mid < end && nums[end] == nums[end - 1]) end--;
+
                     mid++; end--;
                 }
+                else if(sum > 0) end--;
+                else mid++;
             }
-         }
-         return list;
+        }
+            return list;
     }
 }
